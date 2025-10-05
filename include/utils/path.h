d9ea5588082c46d9dfbbade55551f9f3c11fb6a5
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2025-10-05 06:22:58 +0100

    [Myles] Removed VSCode file, updated path and makefile

--- include/utils/path.h
+++ include/utils/path.h
@@ -3,7 +3,6 @@
 #include <stdio.h>
 #include <utils/terminal.h>
 #include <linux/limits.h>
-#include <stdlib.h>
 
 const char *get_current_exec_path(char *buf, size_t buf_size);
 const char *get_home_path(void);
