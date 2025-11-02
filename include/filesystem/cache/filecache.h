0ab970df615b5b0b34df786553d16efe2b6e1b14
Myles "Mellurboo" Wilson <mellurblur@gmail.com>
2025-11-02 11:00:49 +0000

    [Myles] File Caching "API"
    Functions for File Cacheing have been added, not implemented mainstream

---
+++ include/filesystem/cache/filecache.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <stddef.h>
+#include <time.h>
+
+
+
+/// @brief Individual File Cache Entries
+typedef struct {
+    char *path;
+    char *data;
+    size_t size;
+    time_t last_modified;
+} file_cache_entry_t;
+
+/// @brief (Collection of File Cache entries) structure
+typedef struct {
+    file_cache_entry_t *entries;
+    size_t count;
+    size_t capacity;
+    GTMutex threadlock;
+} file_cache_t;
+
+/// @brief find file_cache_entry in file_cache context
+/// @param cache cache context
+/// @param path path of file
+/// @return file_cache_entry index of filepath if cached, -1 indicates failure
+static ssize_t cache_find_entry(file_cache_t *cache, const char *path);
+
+/// @brief creates a new cache context of size
+/// @param capacity context size
+/// @return file cache context pointer
+file_cache_t *cache_new(size_t capacity);
+
+/// @brief free a cache context and all of its entries
+/// @param cache cache context
+void cache_free(file_cache_t *cache);
+
+/// @brief Clear the cache, reuable after as we dont the file cache itself, just entries.
+/// @param cache file cache context
+void cache_clear(file_cache_t *cache);
+
+/// @brief gets a file and attempts to read it from cache, if it doesnt, it will read it and cache it
+/// @param cache cache context
+/// @param path filepath
+/// @param size_out size of file
+/// @return file data
+const char *cache_get_file(file_cache_t *cache, const char *path, size_t *size_out);
